5adab2440f96d50676e74e86ba4af9f6e8e5a02d
CharLEE.cpp <117957000+charlie2233@users.noreply.github.com>
2026-03-27 15:02:50 -0700

    Rescue quality lane work in progress

--- src/app/register-quality.js
+++ src/app/register-quality.js
@@ -1,1 +1,7 @@
-export function registerQuality() {}
+import { registerAccessibility } from '../features/a11y/index.js';
+import { registerShortcuts } from '../features/shortcuts/index.js';
+
+export function registerQuality(app) {
+  registerAccessibility(app);
+  registerShortcuts(app);
+}
